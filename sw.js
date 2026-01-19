c3eac8e82eb7d1ae9fcdf993e50d70fe82e959e7
Ben Campbell <hirebencampbell@gmail.com>
2026-01-19 00:46:07 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,11 @@
 
-const CACHE_NAME = 'plot-twisted-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './favicon-32.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
-  );
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('pt-v1').then(cache=>cache.addAll([
+    '/',
+    '/index.html',
+    '/manifest.json'
+  ])));
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
